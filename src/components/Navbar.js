daf7ce361970cd1eb59d59de3459448ae0d221b4
Prins Kamariya <kamriyaprince@gmail.com>
2023-01-29 14:14:41 +0530

    Added useSelector to subscribe to state in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+    const items = useSelector((state) => state.cart);
     return (
         <div
             style={{
@@ -18,7 +20,7 @@ const Navbar = () => {
                 <Link className="navLink" to="/cart">
                     Cart
                 </Link>
-                <span className="cartCount">CART ITEMS: 0</span>
+                <span className="cartCount">CART ITEMS: {items.length}</span>
             </div>
         </div>
     );
