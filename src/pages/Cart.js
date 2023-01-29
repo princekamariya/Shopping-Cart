3ff509cb0ca7307e9f212ae96a768195f5c83808
Prins Kamariya <kamriyaprince@gmail.com>
2023-01-29 18:08:36 +0530

    used cart state in Cart component and rendered all products from cart state  into Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,25 @@
+/* eslint-disable array-callback-return */
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-    return <div>Cart</div>;
+    const products = useSelector((state) => state.cart);
+
+    return (
+        <div>
+            <h3>Cart</h3>
+            <div className="cartWrapper">
+                {products.map((product) => (
+                    <div className="cartCard">
+                        <img src={product.image} alt="" />
+                        <h5>{product.title}</h5>
+                        <h5>{product.price}</h5>
+                        <button className="btn">Remove</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Cart;
