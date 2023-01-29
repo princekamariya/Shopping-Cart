6e00e200d142b5b9851f6289ef19965eefdd3e91
Prins Kamariya <kamriyaprince@gmail.com>
2023-01-29 13:35:34 +0530

    Added cartSlice and store

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/cart" element={<Cart />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <span className="logo">REDUX STORE</span>
-      <div>
-        <Link className="navLink" to="/">
-          Home
-        </Link>
-        <Link className="navLink" to="/cart">
-          Cart
-        </Link>
-        <span className="cartCount">CART ITEMS: 0</span>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+            }}
+        >
+            <span className="logo">REDUX STORE</span>
+            <div>
+                <Link className="navLink" to="/">
+                    Home
+                </Link>
+                <Link className="navLink" to="/cart">
+                    Cart
+                </Link>
+                <span className="cartCount">CART ITEMS: 0</span>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
