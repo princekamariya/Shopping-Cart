fd44f45181b97e810dcb814f704194bd89f12981
Prins Kamariya <kamriyaprince@gmail.com>
2023-01-30 21:35:09 +0530

    Added cart remove method and completed whole cart app

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,14 @@
 /* eslint-disable array-callback-return */
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../store/cartSlice";
 
 const Cart = () => {
+    const dispatch = useDispatch();
     const products = useSelector((state) => state.cart);
-
+    const handleRemove = (productId) => {
+        dispatch(remove(productId));
+    };
     return (
         <div>
             <h3>Cart</h3>
@@ -14,7 +18,14 @@ const Cart = () => {
                         <img src={product.image} alt="" />
                         <h5>{product.title}</h5>
                         <h5>{product.price}</h5>
-                        <button className="btn">Remove</button>
+                        <button
+                            onClick={() => {
+                                handleRemove(product.id);
+                            }}
+                            className="btn"
+                        >
+                            Remove
+                        </button>
                     </div>
                 ))}
             </div>
